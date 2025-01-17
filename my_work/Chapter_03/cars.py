866f6ccafae33a255c2fe7df5c311922e774a1b4
Pustotely <adresanetu@live.ru>
2025-01-17 17:15:24 +0300

    Countries excersise done

--- my_work/Chapter_03/cars.py
+++ my_work/Chapter_03/cars.py
@@ -4,3 +4,14 @@ print(cars)
 
 cars.sort(reverse=True)
 print(cars)
+
+cars = ['toyota', 'bmw', 'lada', 'subaru']
+print(cars)
+print('------')
+print(sorted(cars))
+print(cars)
+print('------')
+cars.reverse()
+print(cars)
+print('------')
+len(cars)
---
+++ my_work/Chapter_03/ex_countries.py
@@ -0,0 +1,34 @@
+countries = ['Japan', 'Switzerland', 'USA', 'Australia', 'Poland']
+print(countries)
+print('------')
+
+print(sorted(countries))
+print('------')
+
+print(countries)
+print('------')
+
+print(sorted(countries, reverse=True))
+print('------')
+
+print(countries)
+print('------')
+
+countries.reverse()
+print(countries)
+print('------')
+
+countries.reverse()
+print(countries)
+print('------')
+
+countries.sort()
+print(countries)
+print('------')
+
+countries.sort(reverse=True)
+print(countries)
+print('------')
+
+print(len(countries))
+print('------')
