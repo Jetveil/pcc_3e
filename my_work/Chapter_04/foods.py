f997687917cb8d2a2c5f79e38e798979191b6082
Pustotely <adresanetu@live.ru>
2025-01-19 17:14:59 +0300

    Minor changes

---
+++ my_work/Chapter_04/foods.py
@@ -0,0 +1,12 @@
+my_foods = ['mashed potatoes', 'ice cream', 'kuksu']
+friend_foods = my_foods[:]
+print('My foods:')
+print(my_foods)
+print("Friend's foods:")
+print(friend_foods)
+my_foods.append('burger')
+friend_foods.append('soup')
+print('My foods:')
+print(my_foods)
+print("Friend's foods:")
+print(friend_foods)
