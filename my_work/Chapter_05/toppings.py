6cff22e365d9395b1e5c55de230ef89628c9a63e
pustotely <adresanetu@live.ru>
2025-01-29 23:55:58 +0300

    toppings.py modified

--- my_work/Chapter_05/toppings.py
+++ my_work/Chapter_05/toppings.py
@@ -12,9 +12,20 @@
 # for topping in requested_toppings:
 #     print(f'Adding {topping} to you pizza!')
 
-requested_toppings = ['mushrooms', 'jalapeno', 'pepperoni']
+# requested_toppings = ['mushrooms', 'jalapeno', 'pepperoni']
+# if requested_toppings:
+#     for topping in requested_toppings:
+#         if topping == 'jalapeno':
+#             print('Sorry, we are out of jalapeno')
+#         else:
+#             print(f'Adding {topping} to you pizza!')
+# else:
+#     print('Are you sure that you want a plain pizza?')
+
+avaliable_toppings = ['mushrooms', 'jalapeno', 'pepperoni']
+requested_toppings = ['mushrooms', 'ice cream', 'pepperoni']
 for topping in requested_toppings:
-    if topping == 'jalapeno':
-        print('Sorry, we are out of jalapeno')
+    if topping in avaliable_toppings:
+        print(f'Adding {topping} to your pizza')
     else:
-        print(f'Adding {topping} to you pizza!')
+        print(f'Sorry, we dont have {topping} topping')
