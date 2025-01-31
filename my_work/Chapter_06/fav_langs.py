3a42c88d2929eb45189d0f7cb41161980d082682
Pustotely <adresanetu@live.ru>
2025-01-31 17:04:59 +0300

    rndm

---
+++ my_work/Chapter_06/fav_langs.py
@@ -0,0 +1,10 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+}
+
+lang = favorite_languages['sarah'].title()
+
+print(f'Sarah\'s favorite language is {lang}')
