6a0c1678e5c9785acb2e21e700be812bb8722780
Pustotely <adresanetu@live.ru>
2025-02-16 16:58:30 +0300

    .....

--- my_work/Chapter_06/ex_06.py
+++ my_work/Chapter_06/ex_06.py
@@ -41,18 +41,52 @@
 
 # ex 6.4
 
-glossary = {
-    'Conditions': 'If Else statements',
-    'Lists': 'Changable sets of data []',
-    'Tuples': 'Unchangable sets of data ()"',
-    'Dictionaries': 'List of data with Key - Value format',
-    '"keys()" method': 'loops throug dict. keys',
-    '"values()" method': 'loops throug dict. values',
-    '"items()" method': 'loops throug dict. key-val pairs'
+# glossary = {
+#     'Conditions': 'If Else statements',
+#     'Lists': 'Changable sets of data []',
+#     'Tuples': 'Unchangable sets of data ()"',
+#     'Dictionaries': 'List of data with Key - Value format',
+#     '"keys()" method': 'loops throug dict. keys',
+#     '"values()" method': 'loops throug dict. values',
+#     '"items()" method': 'loops throug dict. key-val pairs'
+# }
+
+# for k, v in glossary.items():
+#     if k.endswith('method'):
+#         print(f"{k} is: {v}")
+#     else:
+#         print(f"{k} are: {v}")
+
+# ex 6.5
+
+# locations = {
+#     'nile': 'egypt',
+#     'volga': 'russia',
+#     'yangtze': 'china'
+# }
+
+# for k, v in locations.items():
+#     print(f"{k.title()} flows through {v.title()}")
+# print('-----')
+# for k in locations.keys():
+#     print(f"{k.title()}")
+# print('-----')
+# for v in locations.values():
+#     print(f"{v.title()}")
+
+# ex 6.6
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
 }
 
-for k, v in glossary.items():
-    if k.endswith('method'):
-        print(f"{k} is: {v}")
+poll_ppl = ['jen', 'sarah', 'edward', 'phil', 'alina', 'mark']
+
+for name in poll_ppl:
+    if name in favorite_languages:
+        print(f"{name.title()}, thank you for taking the poll")
     else:
-        print(f"{k} are: {v}")
+        print(f"{name.title()}, would you like to take a poll?")
