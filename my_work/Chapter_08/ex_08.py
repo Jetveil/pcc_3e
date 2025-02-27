4e3941bf6e062a681ab0c74f693e5b1842eee8e0
Pustotely <adresanetu@live.ru>
2025-02-27 18:00:15 +0300

    chapter 08 in progress

--- my_work/Chapter_08/ex_08.py
+++ my_work/Chapter_08/ex_08.py
@@ -1,7 +1,36 @@
 # --- 8.2
 
-def favorite_book(title):
-    print(f"One of my favorite books is {title}")
+# def favorite_book(title):
+#     print(f"One of my favorite books is {title}")
 
 
-favorite_book('Alice in the Wonderland')
+# favorite_book('Alice in the Wonderland')
+
+# --- 8.3
+
+# def make_shirt(size, text):
+#     print(f"The {size} size t-shirt with printed text: \"{text}\"")
+
+
+# make_shirt(48, 'cocksucker')
+# make_shirt(text='pussy_destroyer', size=38)
+
+
+# --- 8.4
+
+# def make_shirt(size='L', text='I love Python'):
+#     print(f"The {size} size t-shirt with printed text: \"{text}\"")
+
+
+# make_shirt()
+# make_shirt(48, 'CUMMER')
+
+# --- 8.5
+
+# def describe_city(city, country='portugal'):
+#     print(f"City {city.title()} is located in {country.title()}")
+
+
+# describe_city('lissabon')
+# describe_city('moscow', 'russia')
+# describe_city(city='seul', country='south korea')
--- my_work/Chapter_08/functions.py
+++ my_work/Chapter_08/functions.py
@@ -40,9 +40,49 @@
 
 # --- Значения по умолчанию
 
-def describe_pet(pet_name, pet_type='cat'):
-    print(f"I have a {pet_type} with the name of {pet_name.title()}.")
+# def describe_pet(pet_name, pet_type='cat'):
+#     print(f"I have a {pet_type} with the name of {pet_name.title()}.")
 
 
-describe_pet(pet_name='markiz')
-describe_pet(pet_type='dog', pet_name='buba')
+# describe_pet(pet_name='markiz')
+# describe_pet(pet_type='dog', pet_name='buba')
+
+
+# --- Возвращаемое значение (return value)
+
+# def get_formatted_name(first_name, last_name):
+#     full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+
+# musician = get_formatted_name('jimi', 'hendrix')
+
+# print(musician)
+
+# --- Необязательные аргументы
+
+# def get_formatted_name(first_name, last_name, middle_name=''):
+#     if middle_name:
+#         full_name = f"{first_name} {middle_name} {last_name}"
+#     else:
+#         full_name = f"{first_name} {last_name}"
+#     return full_name.title()
+
+
+# dude = get_formatted_name('john', 'suckerman', 'bart')
+# musician = get_formatted_name('michael', 'cocksun', 'lee')
+# print(musician)
+# print(dude)
+
+# --- Возвращение словаря
+
+def build_person(first_name, last_name, age=''):
+    person = {'first': first_name, 'last': last_name}
+
+    if 'age':
+        person['age'] = age
+    return person
+
+
+musician = build_person('jimi', 'hendrix', age=27)
+print(musician)
