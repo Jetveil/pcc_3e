48b168367f522bd605d774226c84bf747163e8b3
Pustotely <adresanetu@live.ru>
2025-03-08 16:40:20 +0300

    ex 9.5 done

--- my_work/Chapter_09/Classes.py
+++ my_work/Chapter_09/Classes.py
@@ -27,3 +27,183 @@
 # your_dog = Dog('Zeigbee', 6)
 # print(f"\nYour dog's name is {your_dog.name} and it's age is {your_dog.age}")
 # your_dog.roll_over()
+
+# --- Работа с классами. Класс Car
+
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+
+#     def describe_car(self):
+#         fullName = f"{self.make}, {self.model}, {self.year}"
+#         return fullName.title()
+
+
+# myCar = Car('audi', 'quattro', 1999)
+# print(myCar.describe_car())
+
+# --- Назначение атрибуту значения по умолчанию
+
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0 # <-- Here
+
+#     def describe_car(self):
+#         fullName = f"\n{self.make}, {self.model}, {self.year}"
+#         return fullName.title()
+
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} kilometeres on it")
+
+
+# myCar = Car('audi', 'quattro', 1999)
+
+# print(myCar.describe_car())
+# myCar.read_odometer()
+
+
+# --- Изменение значений атрибутов
+# -- Прямое изменение значения
+
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+
+#     def describe_car(self):
+#         fullName = f"\n{self.make}, {self.model}, {self.year}"
+#         return fullName.title()
+
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} kilometeres on it")
+
+
+# myCar = Car('audi', 'quattro', 1999)
+
+# print(myCar.describe_car())
+# myCar.odometer_reading = 23 # <-- Here
+# myCar.read_odometer()
+
+# -- С помощью метода
+
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 5
+
+#     def describe_car(self):
+#         fullName = f"\n{self.make}, {self.model}, {self.year}"
+#         return fullName.title()
+
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} kilometeres on it")
+
+#     def update_odometer(self, mileage):  # <-- Here
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can't roll back the odometer!")
+
+
+# myCar = Car('audi', 'quattro', 1999)
+
+# print(myCar.describe_car())
+# myCar.update_odometer(2)  # <-- Here
+# myCar.read_odometer()
+
+# -- Изменение значения атрибута с приращением
+
+# --- Изменение значений атрибутов
+# -- Прямое изменение значения
+
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 0
+
+#     def describe_car(self):
+#         fullName = f"\n{self.make}, {self.model}, {self.year}"
+#         return fullName.title()
+
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} kilometeres on it")
+
+
+# myCar = Car('audi', 'quattro', 1999)
+
+# print(myCar.describe_car())
+# myCar.odometer_reading = 23 # <-- Here
+# myCar.read_odometer()
+
+# -- С помощью метода
+
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 5
+
+#     def describe_car(self):
+#         fullName = f"\n{self.make}, {self.model}, {self.year}"
+#         return fullName.title()
+
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} kilometeres on it")
+
+#     def update_odometer(self, mileage):  # <-- Here
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can't roll back the odometer!")
+
+
+# myCar = Car('audi', 'quattro', 1999)
+
+# print(myCar.describe_car())
+# myCar.update_odometer(2)  # <-- Here
+# myCar.read_odometer()
+
+# -- Изменение атрибута с приращением
+
+# class Car:
+#     def __init__(self, make, model, year):
+#         self.make = make
+#         self.model = model
+#         self.year = year
+#         self.odometer_reading = 5
+
+#     def describe_car(self):
+#         fullName = f"\n{self.make}, {self.model}, {self.year}"
+#         return fullName.title()
+
+#     def read_odometer(self):
+#         print(f"This car has {self.odometer_reading} kilometeres on it")
+
+#     def update_odometer(self, mileage):
+#         if mileage >= self.odometer_reading:
+#             self.odometer_reading = mileage
+#         else:
+#             print("You can't roll back the odometer!")
+
+#     def increment_odometer(self, miles):
+#         self.odometer_reading += miles
+
+
+# myCar = Car('audi', 'quattro', 1999)
+
+# print(myCar.describe_car())
+# myCar.update_odometer(10)
+# myCar.increment_odometer(100)
+# myCar.read_odometer()
--- my_work/Chapter_09/ex_09.py
+++ my_work/Chapter_09/ex_09.py
@@ -44,12 +44,78 @@
 
 # --- 9.3
 
+# class User:
+#     def __init__(self, first_name, last_name, user_name, user_age):
+#         self.name = first_name.title()
+#         self.surname = last_name.title()
+#         self.username = user_name
+#         self.age = user_age
+
+#     def describe_user(self):
+#         print(f"\nUser name: {self.name}")
+#         print(f"User last name: {self.surname}")
+#         print(f"User nickname: {self.username}")
+#         print(f"User age: {self.age}")
+
+#     def greet_user(self):
+#         print(f"Hello, {self.name} {self.surname} as {self.username}")
+
+
+# ermak = User('dim', 'erm', 'pustotely', 31)
+# dudar = User('gosha', 'dudar', 'cock teller', 27)
+
+# ermak.describe_user()
+# ermak.greet_user()
+
+# dudar.describe_user()
+# dudar.greet_user()
+
+
+# --- 9.4
+
+# class Restaurant:
+#     def __init__(self, restaurant_name, restaurant_cuisine):
+#         self.restaurant_name = restaurant_name.title()
+#         self.restaurant_cuisine = restaurant_cuisine
+#         self.number_surved = 0
+
+#     def describe_restaurant(self):
+#         print(f"Restaurant name: {self.restaurant_name}")
+#         print(f"Restaurant cuisine: {self.restaurant_cuisine} ")
+#         print(f"Number surved: {self.number_surved}")
+
+#     def open_restaurant(self):
+#         print(f"The Restaurant {self.restaurant_name} is open")
+
+#     def set_number_surved(self, numberServed):
+#         self.number_surved = numberServed
+
+#     def increment_number_surved(self, incrementNumberSurved):
+#         self.number_surved += incrementNumberSurved
+
+
+# restaurant = Restaurant('babaganush', 'Jewish')
+
+# restaurant.set_number_surved(5)
+# restaurant.increment_number_surved(40)
+# restaurant.describe_restaurant()
+# restaurant.open_restaurant()
+
+# --- 9.5
+
 class User:
     def __init__(self, first_name, last_name, user_name, user_age):
         self.name = first_name.title()
         self.surname = last_name.title()
         self.username = user_name
         self.age = user_age
+        self.login_attempts = 0
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
 
     def describe_user(self):
         print(f"\nUser name: {self.name}")
@@ -66,6 +132,11 @@ dudar = User('gosha', 'dudar', 'cock teller', 27)
 
 ermak.describe_user()
 ermak.greet_user()
+ermak.increment_login_attempts()
+ermak.increment_login_attempts()
+print(f"Login attempts: {ermak.login_attempts}")
+ermak.reset_login_attempts()
+print(f"Login attempts: {ermak.login_attempts}")
 
-dudar.describe_user()
-dudar.greet_user()
+# dudar.describe_user()
+# dudar.greet_user()
