00f2a7f52503d464a735ec67d9e074333acc7ad9
pustotely <adresanetu@live.ru>
2025-03-11 22:29:41 +0300

    9.6 in progress

--- my_work/Chapter_09/ex_09.py
+++ my_work/Chapter_09/ex_09.py
@@ -143,3 +143,37 @@
 
 
 # --- ex 9.6
+
+class Restaurant:
+    def __init__(self, restaurant_name, restaurant_cuisine):
+        self.restaurant_name = restaurant_name.title()
+        self.restaurant_cuisine = restaurant_cuisine
+
+    def describe_restaurant(self):
+        print(f"Restaurant name: {self.restaurant_name}")
+        print(f"Restaurant cuisine: {self.restaurant_cuisine} ")
+
+    def open_restaurant(self):
+        print(f"The Restaurant {self.restaurant_name} is open")
+
+
+class IceCreamStand(Restaurant):
+    def __init__(self, restaurant_name, restaurant_cuisine, flavors):
+        super().__init__(restaurant_name, restaurant_cuisine)
+        self.flavors = flavors
+
+    def show_flavors(flavors):
+        flavors = []
+        for flavor in flavors:
+            flavors.append(flavor)
+        return flavors
+
+
+# restaurant = Restaurant('babaganush', 'Jewish')
+ice_cream_stand = IceCreamStand('CreamSpy', 'International', [
+                                'cram', 'amend', 'coca-cola'])
+
+print(ice_cream_stand.show_flavors('cram'))
+
+# restaurant.describe_restaurant()
+# restaurant.open_restaurant()
