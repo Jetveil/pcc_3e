6c600fe4fe591ee33e014b9f474741337abddc34
Pustotely <adresanetu@live.ru>
2025-03-12 15:59:10 +0300

    ex 9.9 done (сложна)

--- my_work/Chapter_09/ex_09.py
+++ my_work/Chapter_09/ex_09.py
@@ -144,36 +144,180 @@
 
 # --- ex 9.6
 
-class Restaurant:
-    def __init__(self, restaurant_name, restaurant_cuisine):
-        self.restaurant_name = restaurant_name.title()
-        self.restaurant_cuisine = restaurant_cuisine
+# class Restaurant:
+#     def __init__(self, restaurant_name, restaurant_cuisine):
+#         self.restaurant_name = restaurant_name.title()
+#         self.restaurant_cuisine = restaurant_cuisine
 
-    def describe_restaurant(self):
-        print(f"Restaurant name: {self.restaurant_name}")
-        print(f"Restaurant cuisine: {self.restaurant_cuisine} ")
+#     def describe_restaurant(self):
+#         print(f"Restaurant name: {self.restaurant_name}")
+#         print(f"Restaurant cuisine: {self.restaurant_cuisine} ")
 
-    def open_restaurant(self):
-        print(f"The Restaurant {self.restaurant_name} is open")
-
-
-class IceCreamStand(Restaurant):
-    def __init__(self, restaurant_name, restaurant_cuisine, flavors):
-        super().__init__(restaurant_name, restaurant_cuisine)
-        self.flavors = flavors
-
-    def show_flavors(flavors):
-        flavors = []
-        for flavor in flavors:
-            flavors.append(flavor)
-        return flavors
+#     def open_restaurant(self):
+#         print(f"The Restaurant {self.restaurant_name} is open")
 
 
-# restaurant = Restaurant('babaganush', 'Jewish')
-ice_cream_stand = IceCreamStand('CreamSpy', 'International', [
-                                'cram', 'amend', 'coca-cola'])
+# class IceCreamStand(Restaurant):
+#     def __init__(self, restaurant_name, restaurant_cuisine):
+#         super().__init__(restaurant_name, restaurant_cuisine)
+#         self.flavors = []
 
-print(ice_cream_stand.show_flavors('cram'))
+#     def show_flavors(self):
+#         print(f"Avaliable flavors:")
+#         for flavor in self.flavors:
+#             print(f" - {flavor}")
 
-# restaurant.describe_restaurant()
-# restaurant.open_restaurant()
+
+# ice_cream_stand = IceCreamStand('CreamSpy', 'International')
+# ice_cream_stand.flavors = [
+#     'cream', 'amend', 'coca-cola']
+
+# ice_cream_stand.show_flavors()
+# ice_cream_stand.describe_restaurant()
+
+# --- 9.7
+
+# class User:
+#     def __init__(self, first_name, last_name, user_name, user_age):
+#         self.name = first_name.title()
+#         self.surname = last_name.title()
+#         self.username = user_name
+#         self.age = user_age
+
+#     def describe_user(self):
+#         print(f"\nUser name: {self.name}")
+#         print(f"User last name: {self.surname}")
+#         print(f"User nickname: {self.username}")
+#         print(f"User age: {self.age}")
+
+#     def greet_user(self):
+#         print(f"Hello, {self.name} {self.surname} as {self.username}")
+
+
+# class Admin(User):
+#     def __init__(self, first_name, last_name, user_name, user_age):
+#         super().__init__(first_name, last_name, user_name, user_age)
+#         self.privileges = []
+
+#     def show_privileges(self):
+#         print(f"Administrator privileges:")
+#         for privilege in self.privileges:
+#             print(f"- {privilege}")
+
+
+# admin = Admin('Boss', 'Big', 'System228', 100)
+# admin.privileges = ['ban users', 'delete users',
+#                     'add messages', 'edit messages']
+
+# admin.describe_user()
+# admin.greet_user()
+# admin.show_privileges()
+
+# --- 9.8
+
+# class User:
+#     def __init__(self, first_name, last_name, user_name, user_age):
+#         self.name = first_name.title()
+#         self.surname = last_name.title()
+#         self.username = user_name
+#         self.age = user_age
+
+#     def describe_user(self):
+#         print(f"\nUser name: {self.name}")
+#         print(f"User last name: {self.surname}")
+#         print(f"User nickname: {self.username}")
+#         print(f"User age: {self.age}")
+
+#     def greet_user(self):
+#         print(f"Hello, {self.name} {self.surname} as {self.username}")
+
+
+# class Admin(User):
+#     def __init__(self, first_name, last_name, user_name, user_age):
+#         super().__init__(first_name, last_name, user_name, user_age)
+#         self.privileges = Privileges()
+
+
+# class Privileges():
+#     def __init__(self):
+#         self.privileges = ['ban users', 'delete users',
+#                            'add messages', 'edit messages']
+
+#     def show_privileges(self):
+#         print(f"Administrator privileges:")
+#         for privilege in self.privileges:
+#             print(f"- {privilege}")
+
+
+# admin = Admin('Boss', 'Big', 'System228', 100)
+# admin.describe_user()
+# admin.greet_user()
+# admin.privileges.show_privileges()
+
+
+# --- 9.9
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 5
+
+    def describe_car(self):
+        fullName = f"\n{self.make}, {self.model}, {self.year}"
+        return fullName.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} kilometeres on it")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back the odometer!")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
+    def fill_gas_tank(self, gasAmount):
+        fillGas = f"Filling gas tank with {gasAmount}cbm"
+        return fillGas
+
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year, battery_capacity):
+        super().__init__(make, model, year)
+        # <-- Аргумент для задания значения батареи при создании экземпляра класса
+        self.battery = Battery(battery_capacity)
+
+    def fill_gas_tank(self, gasAmount):
+        print("This car doesn't have a gas tank")
+
+
+class Battery:
+    def __init__(self, battery_capacity):
+        self.battery_capacity = battery_capacity
+
+    def describe_battery(self):
+        print(f"Battery capacity: {self.battery_capacity}kW/h")
+        return self.battery_capacity
+
+    def upgrade_battery(self):
+        if self.battery_capacity != 65:
+            self.battery_capacity = 65
+
+    def get_range(self):
+        if self.battery_capacity == 40:
+            range = 150
+        elif self.battery_capacity == 65:
+            range = 225
+
+        print(f"This car can go about {range}km on a full charge")
+
+
+my_eclectric_car = ElectricCar('Renault', 'Model 5', 2025, 40)
+my_eclectric_car.battery.describe_battery()
+my_eclectric_car.battery.get_range()
+my_eclectric_car.battery.upgrade_battery()
+my_eclectric_car.battery.get_range()
