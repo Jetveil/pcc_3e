1b66c7e556705f47af92dd8e3b0f679001975f78
Pustotely <adresanetu@live.ru>
2025-03-13 10:53:45 +0300

    Simple grok ex added (backwards)

---
+++ my_work/Chapter_09/Class Importing/GrokExcercise/pets.py
@@ -0,0 +1,22 @@
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def bark(self):
+        print(f"{self.name} says: Woof!")
+
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+
+    def meow(self):
+        print(f"{self.name} says: Meow!")
+
+
+class Bird:
+    def __init__(self, name):
+        self.name = name
+
+    def chirp(self):
+        print(f"{self.name} says: Chirp!")
---
+++ my_work/Chapter_09/Class Importing/GrokExcercise/zoo.py
@@ -0,0 +1,9 @@
+import pets
+
+dog = pets.Dog("Rex")
+dog.bark()
+
+cat = pets.Cat("Markiz")
+cat.meow()
+
+bird = pets.Bird()
