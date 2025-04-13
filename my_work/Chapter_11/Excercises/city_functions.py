95554ad7ccb75493720b314ca14c5e10c2714737
Pustotely <adresanetu@live.ru>
2025-04-13 16:35:02 +0300

    last commit

---
+++ my_work/Chapter_11/Excercises/city_functions.py
@@ -0,0 +1,6 @@
+def get_city_country(city, country):
+    user_location = print(f"{city} {country}".title())
+    return user_location
+
+
+get_city_country('moscow', 'russia')
--- my_work/Chapter_11/name_function.py
+++ my_work/Chapter_11/name_function.py
@@ -1,3 +1,6 @@
-def get_formatted_name(first, middle, last):
-    fullname = f"{first} {middle} {last}"
+def get_formatted_name(first, last, middle=''):
+    if middle:
+        fullname = f"{first} {middle} {last}"
+    else:
+        fullname = f"{first} {last}"
     return fullname.title()
